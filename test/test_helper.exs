05167b4139fb75dd367954de889bac8e74a83687
DecafMango <DecafMangoITMO@yandex.ru>
2024-12-08 21:02:15 +0300

    :wrench: fix: Shared in-memory URI

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,2 +1,8 @@
+Application.ensure_all_started(:ecto_sql)
+Application.ensure_all_started(:backend)
+
+Mix.Task.run("ecto.create", ["--quiet"])
+Mix.Task.run("ecto.migrate", ["--quiet"])
+
 ExUnit.start()
 Ecto.Adapters.SQL.Sandbox.mode(Backend.Repo, :manual)
