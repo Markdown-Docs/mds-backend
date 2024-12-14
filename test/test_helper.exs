09ed5e91016d62d55726b27bd8d5e0c557aece95
worthant <b_dvorkin@niuitmo.ru>
2024-12-14 05:01:08 +0300

    :test_tube: test(mds_converter): Add tests for mds parser

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -5,4 +5,7 @@ Mix.Task.run("ecto.create", ["--quiet"])
 Mix.Task.run("ecto.migrate", ["--quiet"])
 
 ExUnit.start()
+# Code.require_file("support/channel_case.ex", __DIR__)
+Code.require_file("support/data_case.ex", __DIR__)
+Code.require_file("support/conn_case.ex", __DIR__)
 Ecto.Adapters.SQL.Sandbox.mode(Backend.Repo, :manual)
